1621e9bb65f651fc60677b48564c3d24a3e314c6
unknown <786391600@qq.com>
2018-12-29 18:10:59 +0800

    new

--- miniprogram/pages/commodityList/index.js
+++ miniprogram/pages/commodityList/index.js
@@ -3,10 +3,20 @@ const app = getApp()
 const until = require('../../until/index.js')
 Page({
   data: {
-    
+    listData:[]
   },
   onLoad: function() {
     var that = this;
-    this.getCommodity()
+    this.getCommodityList()
+  },
+  getCommodityList: function(){
+    var that = this;
+    until.request({
+      action: 'app.commodity.getCommodityList',
+      data: {}
+    }).then(function (e) {
+      console.log(e.data.data)
+      that.setData({listData: e.data.data})
+    })
   }
 })
