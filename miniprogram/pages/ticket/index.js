2c7745cc0bc46de2cd7cc76c3bb20bb17b520711
wangtao <l>
2019-05-04 02:07:02 +0800

    首页

--- miniprogram/pages/ticket/index.js
+++ miniprogram/pages/ticket/index.js
@@ -7,26 +7,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    active: '1', //切换标题 1：活动 2：首页 3：商品,
-    newDate: '',
-    activityList: [],
-    commodityList: [],
-    activityPage: {
-      page1: 1,
-      page2: 1
-    },
-    navHeight: 0,
-    lodingTip: '暂时没有更多数据，稍后再来~'
+    startAddress: '',
+    endAddress: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var that = this;
-    that.setData({ navHeight: app.globalData.navHeight})
-    this.changetime()
-    this.setActivityList({type:'up', page: that.data.activityPage.page1})
+    this.getTicketInfo()
   },
 
   /**
@@ -78,19 +67,26 @@ Page({
 
   },
   /**
-   * 切换首页
+   * 变换地址
    */
-  changestate: function(e) {
-    this.setData({
-      active: e.currentTarget.dataset.active
-    })
-    if (e.currentTarget.dataset.active === '1'){
-      this.data.activityPage.page1 = 1;
-      this.setActivityList({ page: 1, type: 'down' });
+  exchangeAddress: function() {
+    let b;
+    let a = this.data.startAddress;
+    let c = this.data.endAddress;
+    if (a && c) {
+      b = a
+      a = c
+      c = b
+      this.setData({startAddress: a, endAddress: c})
     } else {
-      this.data.activityPage.page2 = 1;
-      this.setCommodityList({ page: 1, type: 'down' });
+      return 
     }
+  },
+  /** 
+   * 获取首页数据
+  */
+  getTicketInfo: function() {
+    this.setData({startAddress: '太原理工', endAddress: '乡宁'})
   },
   //下拉刷新
   onPullDownRefresh: function() {
@@ -99,113 +95,5 @@ Page({
     this.changetime();
     this.data.activityPage.page1 = 1;
     this.setActivityList({ page: 1, type: 'down' });
-  },
-  /**
-   * 改变时间
-   */
-  changetime: function() {
-    var data = new Date()
-    var year = data.getFullYear()
-    var month = data.getMonth() + 1
-    var day = data.getDate()
-    var time = [year, month, day].map(formatNumber).join('/')
-    function formatNumber(n) {
-      n = n.toString()
-      return n[1] ? n : '0' + n
-    }
-    console.log(time)
-    this.setData({
-      newDate: time //改变时间
-    })
-  },
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-    var that = this;
-    this.data.activityPage.page1++
-    this.setActivityList({ page: that.data.activityPage.page1, type: 'up'});
-  },
-  getActivityList: function(query){
-    var query = query || {};
-    return new Promise((resolve, reject) => {
-      console.log(query)
-      until.request({
-        action: 'app.activity.getActivityList',
-        data: query
-      }).then(function (e) {
-        if (e.data.success){
-          resolve(e.data.data)
-        }
-      }) 
-    })
-  },
-  getCommodityList: function (query) {
-    var that = this;
-    var query = query || {}
-    return new Promise((resolve, reject)=>{
-      until.request({
-        action: 'app.commodity.getCommodityList',
-        data: query
-      }).then(function (e) {
-        if (e.data.success) {
-          resolve(e.data.data)
-        }
-      })
-    })
-  },
-  getActivityInfo (e) {
-    var data = e.currentTarget.dataset.details;
-    wx.navigateTo({
-      url: '/pages/activity/index?activityId='+data.id
-    })
-  },
-  getCommodityInfo (e) {
-    var data = e.currentTarget.dataset.details;
-    wx.navigateTo({
-      url: "/pages/commodity/index?clickId=" + data.id
-    })
-  },
-  setActivityList (obj) {
-    var that = this;
-    // obj.page 页数  type 上拉 下滑
-    var page = obj.page || 1;
-    var type = obj.type || 'up'
-    that.setData({ lodingTip: "玩命加载中！" })
-    this.getActivityList({ page: page}).then((data) => {
-      if (data && data.length > 0) {
-        if (type === 'up') {
-          var list = that.data.activityList.concat(data)
-        } else {
-          wx.stopPullDownRefresh()
-          var list = data;
-        }
-        that.setData({ activityList: list })
-      } else {
-        that.data.activityPage.page1 = (page - 1)!=0 ? page-1 : 1;
-        that.setData({ lodingTip: "暂时没有更多数据，稍后再来~" })
-      }
-    })
-  },
-  setCommodityList (obj) {
-    var that = this;
-    // obj.page 页数  type 上拉 下滑
-    var page = obj.page || 1;
-    var type = obj.type || 'up'
-    that.setData({ lodingTip: "玩命加载中！" })
-    this.getCommodityList({ page: page }).then((data) => {
-      if (data && data.length > 0) {
-        if (type === 'up') {
-          var list = that.data.activityList.concat(data)
-        } else {
-          wx.stopPullDownRefresh()
-          var list = data;
-        }
-        that.setData({commodityList: list })
-      } else {
-        that.data.activityPage.page2 = (page - 1) != 0 ? page - 1 : 1;
-        that.setData({ lodingTip: "暂时没有更多数据，稍后再来~" })
-      }
-    })
   }
 })
