e1d676ed095bc0c1121d5802e580a072a36538af
wangtao <l>
2021-04-20 19:59:25 +0800

    feat: new

--- miniprogram/pages/work/addressList/index.js
+++ miniprogram/pages/work/addressList/index.js
@@ -26,8 +26,10 @@ Page({
     let currentAddress = e.currentTarget.dataset.address
     let pages = getCurrentPages(); //获取当前页面pages里的所有信息。
     let prevPage = pages[pages.length - 2]; //prevPage 是获取上一个页面的js里面的pages的所有信息。 -2 是上一个页面，-3是上上个页面以此类推。
+    let form = prevPage.data.form
+    form.address = currentAddress
     prevPage.setData({  // 将我们想要传递的参数在这里直接setData。上个页面就会执行这里的操作。
-      address: currentAddress
+      form: form
     })
     wx.navigateBack({
       delta: 0
--- miniprogram/pages/work/workForm/index.js
+++ miniprogram/pages/work/workForm/index.js
@@ -4,29 +4,19 @@ const globleData = require('../globleData.js');
 Page({
   data: {
     array: ['1公斤', '2公斤', '3公斤', '4公斤', '5公斤', '6公斤', '7公斤', '8公斤'],
-    objectArray: [
-      {
-        id: 0,
-        name: '美国'
-      },
-      {
-        id: 1,
-        name: '中国'
-      },
-      {
-        id: 2,
-        name: '巴西'
-      },
-      {
-        id: 3,
-        name: '日本'
-      }
-    ],
     index: 0,
     multiArray: [['立即', '今天', '明天', '后天'], [], []],
     multiIndex: [0, 0, 0],
     placeholder: '请输入帮取信息，如xxx驿站，收件人xx，号码xxxxx',
-    address: null
+    address: null,
+    form: {
+      taskTime: 'now',
+      taskContent: '',
+      apFee: 0, // 垫付费用
+      fee: '2',
+      taskFee: '',
+      address: ''
+    }
   },
   onLoad () {
     this.initAddress()
@@ -110,27 +100,86 @@ Page({
   },
   initAddress () {
     const addressList = globleData.getAdressList()
+    let form = this.data.form
+    form.address = addressList[0] || null
     this.setData({
-      address: addressList[0] || null
+      form: form
     })
   },
   payForm () {
-    until.workPay({
-      body: '帮我取',
-      fee: '0.01',
-      type: 'work'
-    }).then((res) => {
-      wx.requestSubscribeMessage({
-        tmplIds: ['QSris88nvy4XR0ldWbXuwOFQM4yfCIdsZsxYLS1_Nos', '6xO6Gbqw-REh6vGjA8eZIueDOEgfFQx9glA3RKdr7pc'],
-        success (res) {}
+    this.computedFee()
+    if (this.valudateForm()) {
+      return
+    } else {
+      until.workPay({
+        body: '帮我取',
+        fee: this.data.form.fee,
+        form: this.data.form,
+        type: 'work'
+      }).then((res) => {
+        wx.requestSubscribeMessage({
+          tmplIds: ['QSris88nvy4XR0ldWbXuwOFQM4yfCIdsZsxYLS1_Nos', '6xO6Gbqw-REh6vGjA8eZIueDOEgfFQx9glA3RKdr7pc'],
+          success (res) {}
+        })
+        wx.navigateTo({
+          url: '/pages/work/paySuccess/index?type=success'
+        })
+      }).catch((res) => {
+        wx.navigateTo({
+          url: '/pages/work/paySuccess/index'
+        })
       })
-      wx.navigateTo({
-        url: '/pages/work/paySuccess/index?type=success'
-      })
-    }).catch((res) => {
-      wx.navigateTo({
-        url: '/pages/work/paySuccess/index'
-      })
+    }
+  },
+  workTimeChange (e) {
+    let valueArr = e.detail.value
+    if (valueArr[0] === 0) {
+      this.data.form.taskTime = 'now'
+    } else {
+      const yyyy = this.getDate().year
+      const mth = this.getDate().month
+      const dd = this.getDate().day + (valueArr[0] - 1)
+      const hh = this.data.multiArray[1][valueArr[1]]
+      const mm = this.data.multiArray[2][valueArr[2]]
+      this.data.form.taskTime = new Date(yyyy,mth,dd,hh,mm, '00').getTime()
+    }
+  },
+  valueChange (e) {
+    let formKey = e.target.dataset.formkey
+    let currentValue = e.detail.value
+    this.data.form[formKey] = currentValue
+  },
+  getDate() {
+    const date = new Date()
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDate()
+    }
+  },
+  valudateForm () {
+    let flag = false
+    const desObj = {
+      taskTime: '预约时间',
+      taskContent: '任务内容',
+      address: '任务地址'
+    }
+    const form = this.data.form
+    Object.keys(desObj).forEach((key) => {
+      if (!form[key]) {
+        flag = true
+        wx.showToast({
+          title: desObj[key] + '必填！',
+          icon: 'error',
+          duration: 2000
+        })
+      }
     })
+    return flag
+  },
+  computedFee () {
+    const fee = parseFloat(this.data.form.fee) * 1000
+    const taskFee = fee - fee * 0.3
+    this.data.form.taskFee = taskFee / 1000
   }
 })
